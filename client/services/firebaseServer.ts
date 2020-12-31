b96456f01ccfb2c17d36363474d60caeb3d18941
mktu <c1ow11er75@gmail.com>
2020-12-31 16:16:46 +0900

    store the credential in environment variables

--- client/services/firebaseServer.ts
+++ client/services/firebaseServer.ts
@@ -2,14 +2,13 @@ import * as firebaseAdmin from 'firebase-admin';
 
 // get this JSON from the Firebase board
 // you can also store the values in environment variables
-import serviceAccount from '../our-bookmarks-18a31-firebase-adminsdk-1hh3k-55fb46f4c9.json'; 
-
+//import serviceAccount from '../our-bookmarks-18a31-firebase-adminsdk-1hh3k-55fb46f4c9.json'; 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
     }),
     databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
   });
