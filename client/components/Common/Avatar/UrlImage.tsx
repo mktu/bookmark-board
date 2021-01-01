95162365bc5d19c9ebafea8476ade40683491f2e
mktu <c1ow11er75@gmail.com>
2021-01-01 13:41:15 +0900

    optimaze image(2)

--- client/components/Common/Avatar/UrlImage.tsx
+++ client/components/Common/Avatar/UrlImage.tsx
@@ -6,7 +6,8 @@ type Props = {
     src ?: string,
     width ?: string,
     height ?: string,
-    name ?: string
+    name ?: string,
+    enableEndpoint ?: boolean
 }
 const UrlEndpoint = process.env.NEXT_PUBLIC_IMGKIT_ID
 
@@ -14,16 +15,17 @@ const UrlImage: React.FC<Props> = ({
     src,
     width,
     height,
-    name = 'Unknown'
+    name = 'Unknown',
+    enableEndpoint = true
 }) => {
-    const [useEndpoint,setUseEndpoint] = useState(true)
+    const [useEndpoint,setUseEndpoint] = useState(enableEndpoint)
     return (
         <div className={styles['url-image-wrapper']} style={{
             width,
             height
         }}>
             {src ? (
-                <img alt={name} width={width} src={useEndpoint ? `${UrlEndpoint}${src}` : src} loading='lazy' onError={(e)=>{
+                <img alt={name} width={width} src={useEndpoint ? `${UrlEndpoint}tr:w-${width}/${src}` : src} loading='lazy' onError={()=>{
                     setUseEndpoint(false)
                 }}/>
             ) : (
