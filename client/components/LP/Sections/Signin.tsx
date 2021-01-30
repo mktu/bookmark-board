23a878e52901091e60ae51e0cea8ef598c1f3840
mktu <c1ow11er75@gmail.com>
2021-01-30 23:02:31 +0900

    bug fix for LP

--- client/components/LP/Sections/Signin.tsx
+++ client/components/LP/Sections/Signin.tsx
@@ -6,10 +6,10 @@ export const Title = '利用を始める'
 export const Content: React.FC<{ handleLogin: () => void }> = ({
     handleLogin
 }) => (
-    <p>
-        <div>Googleのアカウントがあれば無料で開始することができます。早速ブックマークの登録を行いましょう！</div>
+    <div>
+        <p>Googleのアカウントがあれば無料で開始することができます。早速ブックマークの登録を行いましょう！</p>
         <div className='my-4 w-full flex justify-center'>
             <ContainedButton onClick={handleLogin}>サインイン・サインアップする</ContainedButton>
         </div>
-    </p>
+    </div>
 )
--- client/components/LP/Sections/Topics.tsx
+++ client/components/LP/Sections/Topics.tsx
@@ -11,44 +11,44 @@ export const Topics = [
         icon: <FolderOpen className='w-8' />,
         name: 'グループ',
         content: (
-            <p>
-                <div>
+            <div>
+                <p>
                     目的や分野に応じたブックマークグループを最大50まで作成することができます。
                     また、それぞれのグループに対して50までのブックマークを登録することが可能です。一度登録したブックマークを別のグループに移動させることも容易です
-                </div>
+                </p>
                 <div className='relative flex justify-end mt-4' style={{ width: '100%', height: '300px' }}>
                     <LpGroup />
                 </div>
-            </p>
+            </div>
         )
     },
     {
         icon: <Share className='w-8 stroke-primary-main' />,
         name: '共同編集',
         content: (
-            <p>
-                <div>
+            <div>
+                <p>
                     共有したい人をブックマークリストの共同編集者として招待することで、一緒にそのブックマークを充実させることができます。誰かと一緒に情報を収集したい際などに便利です
-                </div>
+                </p>
                 <div className='relative mt-4 flex justify-end' style={{ width: '100%', height: '300px' }}>
                     <LpShare />
                 </div>
-            </p>
+            </div>
         )
     },
     {
         icon: <Template className='w-8 stroke-primary-main' />,
         name: 'カスタマイズ',
         content: (
-            <p className='flex flex-col'>
-                <div>
+            <div className='flex flex-col'>
+                <p>
                     ブックマークに対して、ひとことコメントやカラーリングなどを設定し、コンテンツを自分好みにカスタマイズすることができます。
                     自分なりの色やコメントを記載することで、そのブックマークを識別する手助けとすることができます
-                </div>
+                </p>
                 <div className='relative mt-4 w-full' style={{height:'300px', width : '100%'}}>
                     <LpComment />
                 </div>
-            </p>
+            </div>
         )
     },
 ]
