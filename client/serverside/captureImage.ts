1c33ad648ab45135892d1eae1f678b082f28e836
mktu <c1ow11er75@gmail.com>
2021-02-07 15:42:49 +0900

    add logging for debug

--- client/serverside/captureImage.ts
+++ client/serverside/captureImage.ts
@@ -9,9 +9,8 @@ const md5FromUrl = (url: string) => {
 }
 
 const capture = async (url: string) => {
-    const browser = await puppeteer.launch({
-        product : 'chrome'
-    });
+    console.log(puppeteer._launcher.executablePath())
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url, {
         waitUntil: ['load', 'networkidle0']
