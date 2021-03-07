ac423123180a04e7c209af96f80c83af03863ff8
mktu <c1ow11er75@gmail.com>
2021-03-08 08:48:43 +0900

    changed bookmark home layout

--- client/components/Bookmarks/Group/Home/Presenter.tsx
+++ client/components/Bookmarks/Group/Home/Presenter.tsx
@@ -18,20 +18,24 @@ const Presenter: React.FC<Props> = ({
     return (
         <div className='w-full h-full flex items-center justify-center'>
             <div className='text-primary-main p-4'>
-                <div className='text-xl text-primary-main  mb-12'>
-                    <span className='mx-1'>{userName}</span>
-                    <span>さんの利用状況</span>
+                <div className='flex items-center justify-center flex-col'>
+                    <div className='text-xl text-primary-main  mb-12'>
+                        <span className='mx-1'>{userName}</span>
+                        <span>さんのBookmarks</span>
+                    </div>
+                    <div>
+                        <BookmarkHome width={300} height={187} />
+                    </div>
+                    <div className='my-4'>
+                        <div>登録グループ数 : <span className='text-primary-dark'>{groups} / {MaxGroupNumber}</span> </div>
+                        <div>登録ブックマーク数 : <span className='text-primary-dark'>{bookmarks} / {MaxGroupNumber * MaxBookmarkNumber}</span></div>
+                    </div>
                 </div>
-                <div>
-                    <BookmarkHome width={300} height={187} />
-                </div>
-                <div className='my-4'>
-                    <div>登録グループ数 : <span className='text-primary-dark'>{groups} / {MaxGroupNumber}</span> </div>
-                    <div>登録ブックマーク数 : <span className='text-primary-dark'>{bookmarks} / {MaxGroupNumber * MaxBookmarkNumber}</span></div>
-                    <div>最近登録したブックマーク</div>
-                </div>
-                <div>
-                    {recentCreated}
+                <div className='border-t w-full border-primary-border'>
+                    <h6 className='my-2 font-semibold text-sm'>最近登録したブックマーク</h6>
+                    <div className='px-2'>
+                        {recentCreated}
+                    </div>
                 </div>
             </div>
         </div>
