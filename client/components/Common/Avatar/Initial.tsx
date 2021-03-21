f27ea71259f77d737927889be895d13331353602
mktu <c1ow11er75@gmail.com>
2021-03-21 12:08:15 +0900

    Fixed japanese initial name avatar

--- client/components/Common/Avatar/Initial.tsx
+++ client/components/Common/Avatar/Initial.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import initials from 'initials'
 import classNames from 'classnames'
 
+function ja2Bit ( str:string ) {
+    return ( str.match(/^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/) )? true : false
+}
+
 const bgValues = [
     '#607d8b',
     '#e91e63',
@@ -29,7 +33,7 @@ const Initial: React.FC<Props> = ({
     name = 'Unknown',
     className
 }) => {
-    const ini = initials(name)
+    const ini = ja2Bit(name) ? name[0] : initials(name)
     const colodIdx = (name.length * ini.length) % bgValues.length
     return (
         <div className={classNames(className, `flex items-center justify-center rounded-full text-white p-2 overflow-hidden`)}
