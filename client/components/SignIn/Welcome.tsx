98c712b36c6ad67730729dd4da24cd13399b9c51
mktu <c1ow11er75@gmail.com>
2021-04-04 20:59:16 +0900

    home link to signin

--- client/components/SignIn/Welcome.tsx
+++ client/components/SignIn/Welcome.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { BookmarksSigninImg } from '@components/Common/Image'
 import Logo from '@components/Common/Logo/Logo'
+import { ButtonBase } from '@components/Common/Button'
 import { AppName } from '@utils/constants'
 
 const Welcome = () => {
+    const router = useRouter()
     return (
         <div className='flex flex-col items-center justify-center p-4 h-full'>
-            <div>
+            <ButtonBase onClick={()=>{
+                router.push('./')
+            }}>
                 <Logo theme='dark' size='lg' />
-            </div>
+            </ButtonBase>
             <p className='p-4 md:w-6/12 text-sm my-8'>
                 <span>{AppName}では、気に入ったWEBサイトなどのリンクを保存し、管理することができます。</span>
                 <span>保存したURLは友達や仕事仲間と共有し、ブックマークリストを一緒に充実させましょう！</span>
