e68eb8dc49080579fa6a2d9001d17fb185fd605c
mktu <c1ow11er75@gmail.com>
2021-05-16 12:41:37 +0900

    add a plugin content in LP

--- client/components/LP/Layout/SidebySide.tsx
+++ client/components/LP/Layout/SidebySide.tsx
@@ -1,30 +1,69 @@
 import React, { CSSProperties } from 'react'
 import classNames from 'classnames'
 
-type Props = {
-    title : React.ReactNode | string,
-    content : React.ReactNode,
-    image : React.ReactNode,
-    className ?: string,
-    style?: CSSProperties
+type ContentProps = {
+    title: React.ReactNode | string,
+    content: React.ReactNode,
+    image: React.ReactNode,
 }
 
-const SideBySide = React.forwardRef<HTMLElement, Props>(function sideBySide({
+type Props = {
+    className?: string,
+    style?: CSSProperties,
+    imageLeft?: boolean
+} & ContentProps
+
+
+
+const RightImageLayout: React.FC<ContentProps> = ({
     title,
     content,
     image,
+}) => (
+    <>
+        <div className='md:mx-12 md:w-1/3 break-words flex flex-col justify-center'>
+            <h1 className='text-primary-700 text-xl md:text-2xl font-bold my-4'>{title}</h1>
+            {content}
+        </div>
+        <div className='my-8 md:my-0 overflow-hidden shadow-lg'>
+            {image}
+        </div>
+    </>
+)
+
+const LeftImageLayout: React.FC<ContentProps> = ({
+    title,
+    content,
+    image,
+}) => (
+    <>
+        <div className='break-words flex flex-col justify-center md:hidden'>
+            <h1 className='text-primary-700 text-xl md:text-2xl font-bold my-4'>{title}</h1>
+            {content}
+        </div>
+        <div className='my-8 md:my-0 overflow-hidden shadow-lg'>
+            {image}
+        </div>
+        <div className='mx-12 w-1/3 break-words md:flex flex-col justify-center hidden'>
+            <h1 className='text-primary-700 text-xl md:text-2xl font-bold my-4'>{title}</h1>
+            {content}
+        </div>
+    </>
+)
+
+const SideBySide = React.forwardRef<HTMLElement, Props>(function sideBySide({
     className,
-    style
+    imageLeft = false,
+    style,
+    ...props
 }, ref) {
     return (
         <section style={style} ref={ref} className={classNames('w-full md:flex flex-row p-4 md:p-8 justify-center', className)} >
-            <div className='md:mx-12 md:w-1/3 break-words flex flex-col justify-center'>
-                <h1 className='text-primary-700 text-xl md:text-2xl font-bold my-4'>{title}</h1>
-                {content}
-            </div>
-            <div className='my-8 md:my-0 overflow-hidden shadow-lg'>
-                {image}
-            </div>
+            {imageLeft ? (
+                <LeftImageLayout {...props} />
+            ) : (
+                <RightImageLayout {...props} />
+            )}
         </section>
     )
 })
---
+++ client/components/LP/Sections/Plugin.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { LpPlugin } from '@components/Common/Image/Lp'
+import { SampleUrl } from '@utils/constants'
+
+export const Title = '拡張機能でより便利に'
+
+export const Content: React.ReactNode = (
+    <div>
+        <p>
+            Chrome拡張をインストールすることで、Bookmark-Boardのページに移動しなくとも、
+            閲覧しているページ上でブックマークの追加が行えるようになります。
+        </p>
+        <p className='mt-2'>
+            <span role='img' aria-label='right' className='mr-2'>👉</span>
+            <a href={SampleUrl} target='_blank' rel='noopener noreferrer' className='underline mx-2'>Chromeのストア</a>
+            <span>からインストールすることができます</span>
+        </p>
+    </div>
+)
+
+export const Image = <LpPlugin width={545} height={400} />
