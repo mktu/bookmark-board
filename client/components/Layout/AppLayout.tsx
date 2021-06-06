97c3a4e5d08106c3d16845b290013cee29838112
mktu <c1ow11er75@gmail.com>
2021-06-06 18:16:47 +0900

    fixed mobile bookmark layout(2)

--- client/components/Layout/AppLayout.tsx
+++ client/components/Layout/AppLayout.tsx
@@ -12,10 +12,10 @@ const Layout = ({
     header
 }: Props) => {
     return (
-        <div className='box-border w-screen h-screen md:flex'>
+        <div className='box-border w-screen h-screen flex flex-col md:flex-row'>
             <div className='md:hidden'>{header}</div>
             <div className='hidden md:block'>{sidebar}</div>
-            <div className='md:overflow-hidden md:flex-1 h-full'>{main}</div>
+            <div className='md:overflow-hidden flex-1 md:h-full'>{main}</div>
         </div>
     )
 }
