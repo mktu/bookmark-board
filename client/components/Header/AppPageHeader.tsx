bb68a78c899586be3969473addc51d2d68a8115d
mktu <c1ow11er75@gmail.com>
2021-06-12 14:57:33 +0900

    improve header menu for mobile

--- client/components/Header/AppPageHeader.tsx
+++ client/components/Header/AppPageHeader.tsx
@@ -1,86 +1,143 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { useInView } from 'react-intersection-observer';
 import classNames from 'classnames'
 import styles from './index.module.scss'
 import { useRouter } from 'next/router'
+import { Modal } from 'react-responsive-modal';
 import Book from '@components/Common/Icon/Book'
 import Search from '@components/Common/Icon/Search'
 import Help from '@components/Common/Icon/Help'
 import User from '@components/Common/Icon/User'
-import { PopoverDivContainer } from '@components/Common/Popover'
-import { TooltipDivContainer } from '@components/Common/Tooltip'
-import { ProfileMenu } from '@components/PopoverMenu'
-import { SvgIconButton } from '@components/Common/Button'
+import MenuIcon from '@components/Common/Icon/Menu'
+import FirebaseContext from '@context/FirebaseContext'
+import { SvgIconButton, TextButton } from '@components/Common/Button'
 import { useProfile } from '@modules/profileSlice'
 import { HelpLink } from '@utils/constants'
 import Layout from './Layout'
 
-const Header = () => {
+// only for mobile page
+
+type DialogProps = {
+    children: React.ReactChild,
+    open: boolean,
+    onClose: () => void
+}
+
+const Dialog: React.FC<DialogProps> = ({
+    children,
+    open,
+    onClose
+}) => {
+    return (
+        <Modal open={open} showCloseIcon={false} blockScroll focusTrapped={false} onClose={onClose} classNames={{
+            modal: 'w-full ',
+        }}>
+            {children}
+        </Modal>
+    )
+}
+
+const Menu: React.VFC = () => {
     const router = useRouter()
+    const [openProfile, setOpenProfile] = useState(false)
     const profile = useProfile()
+    const { clientService } = useContext(FirebaseContext)
 
     const app = (
-        <TooltipDivContainer content='Bookmarkアプリ' placement='bottom' className='mr-4 flex items-center'>
-            <SvgIconButton aria-label='Open Bookmark' colorType='none' className={styles['heroicon-button']} onClick={() => {
+        <div className='my-2 flex items-center'>
+            <TextButton aria-label='Open Bookmark' className='flex items-center' onClick={() => {
                 router.push('/bookmarks')
             }}>
-                <Book />
-            </SvgIconButton>
-        </TooltipDivContainer>
+                <Book strokeWidth={1.5} className='w-10 h-10 stroke-primary-main mr-2' />
+                <div>Bookmarkアプリ</div>
+            </TextButton>
+        </div>
     )
 
     const search = (
-        <TooltipDivContainer content='検索' placement='bottom' className='mr-4 md:hidden flex items-center'>
-            <>
-                {/* <SvgIconButton aria-label='Open Search' colorType='none' className={
-                    classNames(styles['heroicon-button'], 'hidden md:block')} onClick={() => {
-                        router.push('/public-search')
-                    }}>
-                    <Search />
-                </SvgIconButton> */}
-                <SvgIconButton aria-label='Open Search' colorType='none' className={
-                    classNames(styles['heroicon-button'])} onClick={() => {
-                        router.push('/search')
-                    }}>
-                    <Search />
-                </SvgIconButton>
-            </>
-        </TooltipDivContainer>
+        <div className='mr-4 md:hidden flex items-center'>
+            <TextButton aria-label='Open Search' colorType='none' className='flex items-center text-primary-main' onClick={() => {
+                router.push('/search')
+            }}>
+                <Search strokeWidth={1.5} className='w-10 h-10 stroke-primary-main mr-2' />
+                <div>検索</div>
+            </TextButton>
+        </div>
     )
 
     const profileMenu = (
-        <PopoverDivContainer
-            placement='bottom-end'
-            className='flex items-center'
-            content={<ProfileMenu />}>
-            <SvgIconButton aria-label='Login Menu' colorType='none' className={styles['heroicon-button']}>
-                <User />
-            </SvgIconButton>
-        </PopoverDivContainer>
+        <div className='my-2'>
+            <div className='flex items-center text-primary-main' >
+                <TextButton aria-label='Login Menu' colorType='none' className='flex items-center text-primary-main' onClick={() => {
+                    setOpenProfile(b => !b)
+                }}>
+                    <User strokeWidth={1.5} className='w-10 h-10 stroke-primary-main mr-2' />
+                    <div>{profile.name}</div>
+
+                </TextButton>
+            </div>
+            <ul className={`${openProfile ? 'opacity-100 h-12' : 'h-0 overflow-hidden opacity-0'} ml-4 my-2  transition-all ease-in-out duration-500 transform`}>
+                <li>
+                    <TextButton className='underline'
+                        onClick={() => {
+                            router.push('/profile')
+                        }}>編集</TextButton>
+                </li>
+                <TextButton className='underline' onClick={() => {
+                    clientService.logout(() => {
+                        router.push('/signin')
+                    })
+                }} >ログアウト</TextButton>
+
+            </ul>
+        </div>
     )
 
     const help = (
-        <TooltipDivContainer content='使い方' placement='bottom' className='mr-4 flex items-center'>
-            <SvgIconButton aria-label='How to use Bookmark' colorType='none' className={styles['heroicon-button']} onClick={() => {
+        <div className='my-2 flex items-center'>
+            <TextButton aria-label='How to use Bookmark' colorType='none' className='flex items-center text-primary-main' onClick={() => {
                 window && window.open(HelpLink, '_blank')
             }}>
-                <Help />
-            </SvgIconButton>
-        </TooltipDivContainer>
+                <Help strokeWidth={1.5} className='w-10 h-10 stroke-primary-main mr-2' />
+                <div>ヘルプ</div>
+            </TextButton>
+        </div>
     )
 
     return (
-        <Layout>
-            {profile.id ? (
-                <div className='flex items-center mr-2'>
-                    {help}
-                    {search}
-                    {app}
-                    {profileMenu}
-                </div>
-            ) : (
-                <div />
-            )}
-        </Layout>
+        <div>
+            {profileMenu}
+            {app}
+            {search}
+            {help}
+        </div>
+    )
+}
+
+const Header: React.VFC = () => {
+    const [open, setOpen] = useState(false)
+    const { ref, inView } = useInView()
+    const menu = (
+        <SvgIconButton aria-label='Open Search' colorType='none' className={
+            classNames(inView ? styles['heroicon-button'] : 'bg-primary-light rounded-full stroke-primary-main p-2 border border-primary-main')} onClick={() => {
+                setOpen(o => !o)
+            }}>
+            <MenuIcon strokeWidth={1.5} className='w-10 h-10'/>
+        </SvgIconButton>
+    )
+
+    return (
+        <>
+            <Layout ref={ref}>
+                {menu}
+            </Layout>
+            <div className={`${inView? 'h-0 overflow-hidden opacity-0' : 'opacity-75 p-2'} fixed top-0 right-0 transition-all ease-in-out duration-500 transform z-50`}>
+                {menu}
+            </div>
+            <Dialog open={open} onClose={() => { setOpen(o => !o) }}>
+                <Menu />
+            </Dialog>
+        </>
     )
 }
 
