8a0e9a7b92aa0a94f0941f73c9430f8486b9f69d
mktu <c1ow11er75@gmail.com>
2021-07-23 12:13:32 +0900

    fixed checkbox style(2)

--- client/components/Bookmarks/Group/BookmarkList/ListItem/Checkbox.tsx
+++ client/components/Bookmarks/Group/BookmarkList/ListItem/Checkbox.tsx
@@ -27,7 +27,7 @@ const Checkbox : React.VFC<Props> = ({onClick, color, checked, className})=>{
                     {backgroundColor : color} : 
                     {borderColor : `rgba(${rgba[0]},${rgba[1]},${rgba[2]},${colorOpacity})`, borderWidth : 1, stroke : color, strokeOpacity : strokeOpacity} : 
                     {}}>
-            <Check className='w-4 h-4 md:w-4 md:h-4' strokeWidth={3} fill='none'/>
+            <Check className='w-5 h-5 md:w-4 md:h-4' strokeWidth={2} fill='none'/>
         </ButtonBase>
     )
 }
