4166acf32bd1dd3537b2b28fa67609941abb5a8a
mktu <c1ow11er75@gmail.com>
2021-08-28 22:29:22 +0900

    dropdown not working

--- client/components/Bookmarks/Group/Bookmark/Move.tsx
+++ client/components/Bookmarks/Group/Bookmark/Move.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Label } from '../../../Common/Label'
-import Dropdowns from '../../../Common/Input/Dropdowns'
-import Checkbox from '../../../Common/Input/Checkbox'
-import { OutlinedButton } from '../../../Common/Button'
+import { Label } from '@components/Common/Label'
+import Dropdowns from '@components/Common/Input/Dropdowns'
+import Checkbox from '@components/Common/Input/Checkbox'
+import { OutlinedButton } from '@components/Common/Button'
 
 type Props = {
-    moveDest ?: BookmarkGroup,
-    copy : boolean,
-    groups : BookmarkGroup[]
-    disabled : boolean,
-    handleSelectMoveDest : (id:string) => void,
-    handleCheckCopy : (checked:boolean) =>void,
-    handleMove : ()=>void
+    moveDest?: BookmarkGroup,
+    copy: boolean,
+    groups: BookmarkGroup[]
+    disabled: boolean,
+    handleSelectMoveDest: (id: string) => void,
+    handleCheckCopy: (checked: boolean) => void,
+    handleMove: () => void
 }
 
 const Move: React.FC<Props> = ({
@@ -23,24 +23,28 @@ const Move: React.FC<Props> = ({
     handleSelectMoveDest,
     handleMove
 }) => (
-        <div>
-            <Label className='mb-4'>グループを移動</Label>
-            <div className='md:flex items-center'>
-                <Dropdowns
-                    placement='bottom'
-                    options={groups.map(g => ({ label: g.name, value: g.id }))}
-                    selected={moveDest?.id}
-                    onSelect={handleSelectMoveDest} />
-                <Checkbox label='コピーを作成' className='ml-2 mt-2' id='copy' onChange={(e) => { handleCheckCopy(e.target.checked) }} />
-                <OutlinedButton
-                    disabled={disabled}
-                    className='block ml-auto'
-                    colorType='secondary'
-                    onClick={handleMove}>
-                    {copy ? 'コピーする' : '移動する'}
-                </OutlinedButton>
-            </div>
+    <div>
+        <Label className='mb-4'>グループを移動</Label>
+        <div className='md:flex items-center'>
+            <Dropdowns
+                className='w-40'
+                poperStyles={{
+                    maxHeight: 200,
+                }}
+                placement='auto-start'
+                options={groups.map(g => ({ label: g.name, value: g.id }))}
+                selected={moveDest?.id}
+                onSelect={handleSelectMoveDest} />
+            <Checkbox label='コピーを作成' className='ml-2 mt-2' id='copy' onChange={(e) => { handleCheckCopy(e.target.checked) }} />
+            <OutlinedButton
+                disabled={disabled}
+                className='block ml-auto'
+                colorType='secondary'
+                onClick={handleMove}>
+                {copy ? 'コピーする' : '移動する'}
+            </OutlinedButton>
         </div>
-    )
+    </div>
+)
 
 export default Move
