b61d124ec9f6678c6e7275494ddb37f1ba28ee28
mktu <c1ow11er75@gmail.com>
2021-11-26 01:04:36 +0900

    dynamic import liff for nextjs

--- client/hooks/useLiff.ts
+++ client/hooks/useLiff.ts
@@ -1,27 +1,32 @@
 import { useState, useEffect, useCallback } from 'react'
-import liff from '@line/liff';
 
 
 const liffId = process.env.NEXT_PUBLIC_LIFF_ID
 
 const useLiff = ()=>{
     const [hasInit,setInit] = useState(false)
+    const [isLineClient,setLineClient] = useState(false)
     const [isLoggedIn, setLoggedIn] = useState(false)
     const initLiff = useCallback(async ()=>{
+        const liff = typeof window !== 'undefined' && (await import('@line/liff')).default;
+        if(!liff){
+            return
+        }
         await liff.init({
             liffId
         });
+        setLoggedIn(liff.isLoggedIn())
+        setLineClient(liff.isInClient())
     },[])
     useEffect(()=>{
         initLiff().then(()=>{
             setInit(true)
-            setLoggedIn(liff.isLoggedIn())
         })
     },[initLiff])
 
     return {
         hasInit,
-        isLineClient : liff.isInClient(),
+        isLineClient,
         isLoggedIn
     }
 }
