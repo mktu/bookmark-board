f5f4c0331f213a82bf40d93d5e03c3d8cfccf28e
mktu <c1ow11er75@gmail.com>
2021-12-17 01:20:40 +0900

    updated date in list(mobile)

---
+++ client/components/Bookmarks/Group/BookmarkList/ListItem/DateInfo.tsx
@@ -0,0 +1,27 @@
+import { getRelativeDate } from '@utils/date'
+import Refresh from '@components/Common/Icon/Refresh'
+
+type Props = {
+    created: number,
+    lastUpdate?: number,
+}
+
+const DateInfo: React.VFC<Props> = ({
+    lastUpdate,
+    created
+}) => {
+    const type = lastUpdate ? '更新' : '作成'
+    const editDate = getRelativeDate(lastUpdate || created)
+    const icon = lastUpdate ? (<Refresh className='w-4 h-4 stroke-primary-main' />) :
+        editDate.type === 'date' ? <span /> :
+            (<span role='img' aria-label='create' className='mr-1'>🎉</span>)
+    return (
+        <div className='flex items-center text-sm text-primary-main'>
+            {icon}
+            <span className='mx-1'>{editDate.msg}</span>
+            <span className='mr-1'>{type}</span>
+        </div>
+    )
+}
+
+export default DateInfo
---
+++ client/utils/date.ts
@@ -0,0 +1,46 @@
+export const numberToDateTime = (time?: number) => {
+    if (!time) return ''
+    return new Date(time).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })
+}
+
+const numberToDateWithOption = (time?: number, option : Intl.DateTimeFormatOptions = {}) => {
+    if (!time) return ''
+    return new Date(time).toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo', ...option })
+}
+
+export const numberToDate = (time?: number) => {
+    if (!time) return ''
+    return new Date(time).toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo' })
+}
+
+export const getRelativeDate = (time:number) : {msg:string, type: 'justnow' | 'minutes' | 'hours' | 'date'} => {
+    const now = Date.now()
+    const diff = now-time
+    const minuteInMsec = 1000 * 60
+    const hourInMsec = minuteInMsec * 60
+    const dayInMsec = hourInMsec * 24
+    if(diff < minuteInMsec){
+        return {
+            msg: 'たった今',
+            type : 'justnow'
+        }
+    }
+    if(diff < hourInMsec){
+        const t = Math.round(diff/minuteInMsec)
+        return {
+            msg : `${t}分前`,
+            type : 'minutes'
+        }
+    }
+    if(diff < dayInMsec){
+        const t = Math.round(diff/hourInMsec)
+        return {
+            msg : `${t}時間前`,
+            type : 'hours'
+        }
+    }
+    return {
+        msg : `${numberToDateWithOption(time)}`,
+        type : 'date'
+    }
+}
