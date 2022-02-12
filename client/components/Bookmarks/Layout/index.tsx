42fc8133a401d8245132707e7cc8405d64103dc9
mktu <mushi9ui@gmail.com>
2022-02-12 21:02:19 +0900

    changed split border design

--- client/components/Bookmarks/Layout/index.tsx
+++ client/components/Bookmarks/Layout/index.tsx
@@ -15,7 +15,6 @@ const Layout = ({
     group,
     showGroup
 }: Props) => {
-    console.log(showGroup)
     return isTouch ? (
         <div className='flex flex-row w-full h-full'>
             <div className={`h-full w-full ${showGroup && 'hidden'}`}>{groups}</div>
@@ -23,12 +22,13 @@ const Layout = ({
         </div>
     ) : (
         <>
-            <Split className='flex flex-row w-full h-full bg-primary-light' gutterSize={4} gutterAlign="center" sizes={[25, 75]} minSize={[256, 512]} 
+            <Split className='flex flex-row w-full h-full bg-primary-light' gutterAlign="center" sizes={[25, 75]} minSize={[256, 512]} 
                 gutter={(_, direction)=>{
                     const gutterElement = document.createElement('div')
-                    gutterElement.className = `gutter gutter-${direction} border-l border-primary-border hover:bg-primary-border`
+                    gutterElement.className = `gutter gutter-${direction} w-[2px] bg-primary-border hover:cursor-col-resize hover:w-4 hover:border-x hover:border-secondary-border hover:bg-secondary-light transition-all delay-300 duration-300 ease-in-out`
                     return gutterElement
                 }}
+                gutterStyle={()=>({})}
             >
                 <div className={`h-full w-full`}>{groups}</div>
                 <div className={`h-full w-full border-primary-border`}>{group}</div>
