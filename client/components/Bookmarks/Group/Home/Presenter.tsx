c190923b09c8d1cbeae5c1aa905e323e022b5e8a
mktu <mushi9ui@gmail.com>
2022-03-15 01:36:40 +0900

    add adon link on bookmark home

--- client/components/Bookmarks/Group/Home/Presenter.tsx
+++ client/components/Bookmarks/Group/Home/Presenter.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Link from 'next/link'
 import { BookmarkHome } from '@components/Common/Image'
-import { MaxGroupNumber, MaxBookmarkNumber } from '@utils/constants'
+import LoginIcon from '@components/Common/Icon/LineLogin'
+import ExternalLink from '@components/Common/Icon/ExternalLink'
+import { MaxGroupNumber, MaxBookmarkNumber, PluginUrl } from '@utils/constants'
 
 type Props = {
     userName: string,
@@ -8,6 +11,8 @@ type Props = {
     bookmarks: number,
     recentCreated: React.ReactNode
 }
+
+const profileUrl = '/profile'
 
 const Presenter: React.FC<Props> = ({
     userName,
@@ -29,6 +34,26 @@ const Presenter: React.FC<Props> = ({
                     <div className='my-4'>
                         <div>登録グループ数 : <span className='text-primary-dark'>{groups} / {MaxGroupNumber}</span> </div>
                         <div>登録ブックマーク数 : <span className='text-primary-dark'>{bookmarks} / {MaxGroupNumber * MaxBookmarkNumber}</span></div>
+                        <div >
+                            連携機能 :
+                            <ul className='px-2 list-disc list-inside'>
+                                <li>
+                                    <a href={PluginUrl} target='_blank' rel='noopener noreferrer' className='inline-flex items-center text-sm text-primary-dark underline'>
+                                        <span>Chrome拡張を利用する</span>
+                                        <ExternalLink className='ml-1 w-5 h-5 stroke-primary-dark' strokeWidth={1.5} />
+                                    </a>
+
+                                </li>
+                                <li>
+                                    <Link href={profileUrl}>
+                                        <a href={profileUrl} className='inline-flex items-center text-sm text-primary-dark underline'>
+                                            <LoginIcon strokeWidth={1.5} className='mr-1 w-5 h-5' />
+                                            <span>LINE連携を行う</span>
+                                        </a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div className='w-full border-t border-primary-border'>
