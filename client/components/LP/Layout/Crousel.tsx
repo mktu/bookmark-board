5b60391e45b935f238364e5d7dcdc84861131fc9
mktu <mushi9ui@gmail.com>
2022-04-19 09:51:28 +0900

    fixed lp accessibility

--- client/components/LP/Layout/Crousel.tsx
+++ client/components/LP/Layout/Crousel.tsx
@@ -3,13 +3,13 @@ import ChevronLeft from '@components/Common/Icon/ChevronLeft'
 import ChevronRight from '@components/Common/Icon/ChevronRight'
 
 export const renderArrowNext = (clickHandler: () => void, hasNext: boolean) => (
-    <SvgIconButton disabled={!hasNext} className='absolute top-1/2 right-0 z-50 w-8 h-8 stroke-2' onClick={clickHandler}>
+    <SvgIconButton aria-label='next' disabled={!hasNext} className='absolute top-1/2 right-0 z-50 w-8 h-8 stroke-2' onClick={clickHandler}>
         <ChevronRight className='stroke-primary-main hover:stroke-primary-dark'/>
     </SvgIconButton>
 )
 
 export const renderArrowPrev = (clickHandler: () => void, hasPrev: boolean) => (
-    <SvgIconButton disabled={!hasPrev} className='absolute top-1/2 left-0 z-50 w-8 h-8 stroke-2' onClick={clickHandler}>
+    <SvgIconButton aria-label='prev' disabled={!hasPrev} className='absolute top-1/2 left-0 z-50 w-8 h-8 stroke-2' onClick={clickHandler}>
         <ChevronLeft className='stroke-primary-main hover:stroke-primary-dark'/>
     </SvgIconButton>
 )
