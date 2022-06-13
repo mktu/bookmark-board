91873ae9b342aed96a2f5fcd0bb3748b436b2abb
mktu <mushi9ui@gmail.com>
2022-06-13 15:52:04 +0900

    add github actions

---
+++ client/tests/example.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Basic test', () => {
+  test('show signin page', async ({ page }) => {
+    await page.goto('http://localhost:3000')
+    await page.locator(`text='ログインまたは登録する'`).click()
+    await page.waitForURL('http://localhost:3000/signin')
+    const l = page.locator(`text=Sign in with Google`)
+    expect(l.isVisible).toBeTruthy()
+  })
+
+  test('show public bookmark page', async ({ page }) => {
+    await page.goto('http://localhost:3000/public-bookmarks/PHnQx8Q7An9JlMr8EwGl')
+    expect(await page.textContent('h1')).toBe('開発全般')
+  })
+})
