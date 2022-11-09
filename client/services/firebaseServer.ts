6bd9a6a43f322ed6ed054dda3faef716b2bc06d2
mktu <mushi9ui@gmail.com>
2022-11-09 17:15:21 +0900

    fixed prevent firebase admin double init

--- client/services/firebaseServer.ts
+++ client/services/firebaseServer.ts
@@ -1,16 +1,16 @@
-import { initializeApp, cert } from 'firebase-admin/app';
+import { initializeApp, getApps, getApp, cert } from 'firebase-admin/app';
 
 // get this JSON from the Firebase board
 // you can also store the values in environment variables
 //import serviceAccount from '../our-bookmarks-18a31-firebase-adminsdk-1hh3k-55fb46f4c9.json'; 
 
-const firebaseAdmin = initializeApp({
+const firebaseAdmin = getApps().length === 0 ? initializeApp({
   credential: cert({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     projectId: process.env.FIREBASE_PROJECT_ID,
   }),
   storageBucket: process.env.NEXT_PUBLIC_FIRESTORE_STORAGE_BUCKET
-});
+}) : getApp();
 
 export { firebaseAdmin };
