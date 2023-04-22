dc2ac3ddcce01bedc4bf163af888c08899b9d0e8
mktu <mushi9ui@gmail.com>
2023-04-22 23:28:00 +0900

    wip update

--- firebase/functions/src/__tests__/scrape.test.ts
+++ firebase/functions/src/__tests__/scrape.test.ts
@@ -21,7 +21,19 @@ describe('scraping tests', () => {
             description: '“Next.js 12とnext-pwaの組み合わせでいつの間にかbad-precaching-responseエラーが...。こちら記載のquick fixでなんとか解消🙇‍♂️ https://t.co/teZvWzj8B3”',
             url: 'https://twitter.com/mktu13/status/1467334593556934659?s=20',
             images: [
-                'https://pbs.twimg.com/profile_images/1376173980625739785/JbhuJSE8_400x400.jpg'
+                'https://pbs.twimg.com/profile_images/1376173980625739785/JbhuJSE8_200x200.jpg'
+            ]
+        }));
+    })
+
+    test('imdb scrpe', async () => {
+        const ret = await scrape('https://www.imdb.com/title/tt1745960/')
+        expect(JSON.stringify(ret)).toBe(JSON.stringify({
+            title: 'Top Gun: Maverick (2022) - IMDb',
+            description: "Top Gun: Maverick: Directed by Joseph Kosinski. With Tom Cruise, Val Kilmer, Miles Teller, Jennifer Connelly. After thirty years, Maverick is still pushing the envelope as a top naval aviator, but must confront ghosts of his past when he leads TOP GUN's elite graduates on a mission that demands the ultimate sacrifice from those chosen to fly it.",
+            url: 'https://www.imdb.com/title/tt1745960/',
+            images: [
+                'https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_FMjpg_UX1000_.jpg'
             ]
         }));
     })
--- firebase/functions/src/scrape.ts
+++ firebase/functions/src/scrape.ts
@@ -1,14 +1,10 @@
-import ogs, { Options, SuccessResult } from 'open-graph-scraper'
+import ogs from 'open-graph-scraper'
 import { https, logger } from "firebase-functions";
 import fetch from 'node-fetch'
 
 const doScrape = async (url: string) => {
-    const options: Options = {
+    const options: Parameters<typeof ogs>[0] = {
         url,
-        timeout: 10000,
-        headers: {
-            "user-agent": 'bot',
-        },
     }
     try {
         return await ogs(options)
@@ -22,15 +18,19 @@ const scrape = async (url: string, validate?: boolean) => {
     const ret = await doScrape(url)
     if (ret.error) {
         logger.error(ret.result.error)
-        throw new https.HttpsError('invalid-argument', ret.result.error)
+        throw new https.HttpsError('invalid-argument', ret.result.error || '')
     }
-    const successData = ret as SuccessResult
+    const successData = ret
     let images: string[] = []
     if (successData.result.ogImage) {
         if (Array.isArray(successData.result.ogImage)) {
             images = successData.result.ogImage.map(v => typeof v === 'string' ? v : v.url)
-        } else {
+        }
+        else if (typeof successData.result.ogImage === 'object') {
             images = [successData.result.ogImage.url]
+        }
+        else {
+            images = [successData.result.ogImage]
         }
     }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
