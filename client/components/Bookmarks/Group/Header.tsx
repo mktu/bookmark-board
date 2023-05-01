ace8b7e153a6c10578ce6612a176405d23c3a512
mktu <mushi9ui@gmail.com>
2023-05-01 14:08:01 +0900

    group icon should be visible in group view of mobile

--- client/components/Bookmarks/Group/Header.tsx
+++ client/components/Bookmarks/Group/Header.tsx
@@ -36,64 +36,65 @@ const Header: React.FC<Props> = ({
     }
 
     return (
-        <div className='flex h-full w-full items-center border-b border-primary-border bg-white p-1' >
-            <div className='flex max-w-full items-center overflow-hidden p-1' >
-                <div className='flex items-center'>
-                    <SvgIconButton className='md:hidden' onClick={handleBack}>
-                        <ArrowLeft strokeWidth={1.0} className='w-6' />
-                    </SvgIconButton>
-                    <div className='hidden md:block'>
-                        <Emoji selected={group.emojiIcon} onSelectEmoji={(emoji) => {
-                            handleSubmit({ emojiIcon: emoji })
-                        }} />
-                    </div>
-                </div>
-                <ButtonBase className='mr-4 ml-2 block flex-1 overflow-hidden truncate text-left text-primary-main' onClick={jumpTo('setting')}>
-                    <div className='max-w-full overflow-hidden truncate font-semibold' >
-                        {group.name}
-                    </div>
-                    <div className='hidden max-w-full overflow-hidden truncate text-sm md:block'>
-                        {group.description}
-                    </div>
-                </ButtonBase>
-            </div>
-            <div className='ml-auto flex items-center justify-end'>
-                {requests.length > 0 && (
-                    <PopoverDivContainer className='px-2' placement='bottom' content={<RequestUsers requests={requests} />}>
+        <div className='h-full w-full border-b border-primary-border bg-white p-1'>
+            <SvgIconButton className='flex items-center gap-1 text-sm text-primary-main underline md:hidden' onClick={handleBack}>
+                <ArrowLeft strokeWidth={1.0} className='w-4' />
+                グループ一覧に戻る
+            </SvgIconButton>
+            <div className='flex w-full items-center' >
+                <div className='flex max-w-full items-center overflow-hidden p-1' >
+                    <div className='flex items-center'>
                         <div>
-                            <SvgFillIconButton aria-label='Show Requesting User' colorType='secondary' className='relative'>
-                                <UserAddFill className='w-8' />
-                                <span className='absolute rounded-full bg-secondary-300 p-1 text-xs text-white' style={{ left: '-10px', top: '-10px' }}>{requests.length}</span>
-                            </SvgFillIconButton>
+                            <Emoji selected={group.emojiIcon} onSelectEmoji={(emoji) => {
+                                handleSubmit({ emojiIcon: emoji })
+                            }} />
                         </div>
-                    </PopoverDivContainer>
-                )}
-                {editors.map(e => (
-                    <PopoverDivContainer key={e.id} content={<UserPopover user={e} />} placement='bottom' className='flex items-center px-1'>
-                        <SvgIconButton aria-label='Show User'>
-                            <Avatar
-                                src={e.image}
-                                width={32}
-                                height={32}
-                                name={e.name}
-                                fallback={<Initial
+                    </div>
+                    <ButtonBase className='mr-4 ml-2 block flex-1 overflow-hidden truncate text-left text-primary-main' onClick={jumpTo('setting')}>
+                        <div className='max-w-full overflow-hidden truncate font-semibold underline' >
+                            {group.name}
+                        </div>
+                        <div className='hidden max-w-full overflow-hidden truncate text-sm md:block'>
+                            {group.description}
+                        </div>
+                    </ButtonBase>
+                </div>
+                <div className='ml-auto flex items-center justify-end'>
+                    {requests.length > 0 && (
+                        <PopoverDivContainer className='px-2' placement='bottom' content={<RequestUsers requests={requests} />}>
+                            <div>
+                                <SvgFillIconButton aria-label='Show Requesting User' colorType='secondary' className='relative'>
+                                    <UserAddFill className='w-8' />
+                                    <span className='absolute rounded-full bg-secondary-300 p-1 text-xs text-white' style={{ left: '-10px', top: '-10px' }}>{requests.length}</span>
+                                </SvgFillIconButton>
+                            </div>
+                        </PopoverDivContainer>
+                    )}
+                    {editors.map(e => (
+                        <PopoverDivContainer key={e.id} content={<UserPopover user={e} />} placement='bottom' className='flex items-center px-1'>
+                            <SvgIconButton aria-label='Show User'>
+                                <Avatar
+                                    src={e.image}
                                     width={32}
                                     height={32}
                                     name={e.name}
-                                />}
-                            />
+                                    fallback={<Initial
+                                        width={32}
+                                        height={32}
+                                        name={e.name}
+                                    />}
+                                />
+                            </SvgIconButton>
+                        </PopoverDivContainer>
+                    ))}
+                    <TooltipDivContainer className='px-2' content='共有' placement='bottom'>
+                        <SvgIconButton aria-label='Share Option' onClick={jumpTo('share')}>
+                            <ShareIcon strokeWidth={2} className='w-6' />
                         </SvgIconButton>
-                    </PopoverDivContainer>
-                ))}
-                <TooltipDivContainer className='px-2' content='共有' placement='bottom'>
-                    <SvgIconButton aria-label='Share Option' onClick={jumpTo('share')}>
-                        <ShareIcon strokeWidth={2} className='w-6' />
-                    </SvgIconButton>
-                </TooltipDivContainer>
-            </div>
-
-
-        </div >
+                    </TooltipDivContainer>
+                </div>
+            </div >
+        </div>
     )
 }
 
